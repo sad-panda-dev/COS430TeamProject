200bac80e7b960b25c462ecc07c84b2ac1cfe0b8
Abdallah Mohamed <96321216+Abdallah-Tah@users.noreply.github.com>
2022-03-10 03:24:54 -0500

    added database and sound

--- Food.py
+++ Food.py
@@ -7,25 +7,17 @@ class Food(object):
 
     '''Constructor for the Food object'''
     def __init__(self):
-        self.__position = (0, 0)
-        self.__color = (204, 0, 0)
+        self.position = (0, 0)
+        self.color = (204, 0, 0)
         self.random_position()
-
-    '''Getter for position property'''
-    def get_position(self):
-        return self.__position
-
-    '''Setter for position property'''
-    def set_position(self, position):
-        self.__position = position
 
     ''' Places food randomly upon every generation'''
     def random_position(self):
-        self.__position = (random.randint(0, s.GRID_WIDTH - 1) * s.GRID_SIZE,
+        self.position = (random.randint(0, s.GRID_WIDTH - 1) * s.GRID_SIZE,
                          random.randint(0, s.GRID_HEIGHT - 1) * s.GRID_SIZE)
 
     ''' set up to be a red square right now can be changed to be sprite later'''
     def draw(self, surface):
-        rect = pg.Rect((self.__position[0], self.__position[1]), (s.GRID_SIZE, s.GRID_SIZE))
-        pg.draw.rect(surface, self.__color, rect)
+        rect = pg.Rect((self.position[0], self.position[1]), (s.GRID_SIZE, s.GRID_SIZE))
+        pg.draw.rect(surface, self.color, rect)
         pg.draw.rect(surface, (93, 216, 228), rect, 1)
--- Snake.py
+++ Snake.py
@@ -4,66 +4,47 @@ import pygame as pg
 import random
 import setup as s
 import sys
+from pathlib import Path
 
 
 class Snake(object):
     ''' Constructor for Snake object
     '''
     def __init__(self):
-        self.__length = 1
-        self.__positions = [((s.SCREEN_WIDTH / 2), (s.SCREEN_HEIGHT / 2))]
-        self.__direction = random.choice([s.UP, s.DOWN, s.LEFT, s.RIGHT])
-        self.__color = (0, 0, 0)
+        self.length = 1
+        self.positions = [((s.SCREEN_WIDTH / 2), (s.SCREEN_HEIGHT / 2))]
+        self.direction = random.choice([s.UP, s.DOWN, s.LEFT, s.RIGHT])
+        self.color = (0, 0, 0)
         # initiate the score to zero
-        self.__score = 0
-
+        self.score = 0
+        
     '''Getter method for first element of position property (snake head)
     '''
     def get_head_position(self):
-        return self.__positions[0]
-
-    '''Getter for length property'''
-    def get_length(self):
-        return self.__length
-
-    '''Getter for positions property'''
-    def get_positions(self):
-        return self.__positions
-    '''Getter for direction'''
-    def get_direction(self):
-        return self.__direction
-    '''Getter for score'''
-    def get_score(self):
-        return self.__score
-
-    '''Function to add to the length of snake'''
-    def add_length(self, integer):
-        self.__length += integer
-
-    '''Function to update score'''
-    def update_score(self, integer):
-        self.__score += integer
-
+        return self.positions[0]
     '''Turn snake 
     '''
     def turn(self, point):
-        if self.__length > 1 and (point[0] * -1, point[1] * -1) == self.__direction:
+        if self.length > 1 and (point[0] * -1, point[1] * -1) == self.direction:
             return
         else:
-            self.__direction = point
+            self.direction = point
 
     # this method is currently where Im trying to figure out how to teleport snake(not functioning)
     def hit_portal(self):
         new_position = self.get_head_position()
+
         # position_variant = random.randint(1, 10)
         # for position in self.positions:
         #     position = position * position_variant
+        
+
         return new_position
     '''Draw the snake object on the surface. Snake is a rect at the moment.
     '''
     def draw(self, surface):
-        for pos in self.__positions:
+        for pos in self.positions:
             rect = pg.Rect((pos[0], pos[1]), (s.GRID_SIZE, s.GRID_SIZE))
-            pg.draw.rect(surface, self.__color, rect)
+            pg.draw.rect(surface, self.color, rect)
             pg.draw.rect(surface, (93, 216, 218), rect, 1)
 
